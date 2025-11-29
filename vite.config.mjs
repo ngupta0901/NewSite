c3f006b31bf65f8669aa23e64aa420651f87a4c7
Naveen Gupta <ngupta0901@gmail.com>
2025-11-29 19:23:00 +0530

    updated pagae.json

--- vite.config.mjs
+++ vite.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig({
     port: "4028",
     host: "0.0.0.0",
     strictPort: true,
+    base: './',
     allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
   }
 });
